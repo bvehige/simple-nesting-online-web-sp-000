3f0537cd5fe28a28c421767180e62b6697307629
Brian Vehige <brianvehige@gmail.com>
2020-02-24 04:59:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,11 @@ def adding_matz
    programmer_hash[:yukihiro_matsumoto] = {
      :known_for => "COBOL",
      :languages => ["LISP", "C"] }
+     
+  programmer_hash
     
 end
-programmer_hash
+
 
 def changing_alan
 	programmer_hash = 
