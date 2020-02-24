c7515d189c68b4d83ee086a9b2b80b9ac23980be
Brian Vehige <brianvehige@gmail.com>
2020-02-24 05:19:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,12 +80,12 @@ def adding_matz
         }
      }
    
-   programmer_hash[:yukihiro_matsumoto] = {}
+   programmer_hash[:yukihiro_matsumoto] = 
    programmer_hash[:yukihiro_matsumoto] = 
     known_for: "Ruby",
    programmer_hash[:languages] = 
    languages: ["LISP", "C"]
-    }
+    
     
   programmer_hash
 end
